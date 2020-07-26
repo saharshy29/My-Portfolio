6207c6ee29ab3826febc870c3804722cdd18b6e8
Afzal Sayed <afzalsayed96@gmail.com>
2020-07-26 21:02:23 +0530

    Updates

---
+++ precache-manifest.246f3421d60168bfb56b495d3ed164de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320d89d5ba59b9de82d81bbd608a2782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a7e14bd5074c3a7f654",
+    "url": "/static/css/main.46be3afe.chunk.css"
+  },
+  {
+    "revision": "b0b18f8339f7b286dec9",
+    "url": "/static/js/2.3f7ab798.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3f7ab798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7e14bd5074c3a7f654",
+    "url": "/static/js/main.82c1b441.chunk.js"
+  },
+  {
+    "revision": "5e02bf1c401296f0f189",
+    "url": "/static/js/runtime-main.07221684.js"
+  }
+]);
